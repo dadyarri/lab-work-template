e73c01782074a3aa426f5715fb958f8af0c341bf
Daniil <dadyarri@gmail.com>
2021-04-21 01:27:43 +0300

    feat: add parsing docstring

--- {{cookiecutter.lab_work_num}}/fill_report_template.py
+++ {{cookiecutter.lab_work_num}}/fill_report_template.py
@@ -1,9 +1,26 @@
+import re
 from pathlib import Path
 
 from docxtpl import DocxTemplate
 
 
 template = DocxTemplate(Path("report{{cookiecutter.lab_work_num}}.docx"))
+source_code = Path("{{cookiecutter.file_name}}.py").open().readlines()
+
+goal = re.search(
+    r"(?<=\"\"\"\nGoal:\n)[\S\s]+(?=EndGoal)",
+    "".join(source_code),
+).group(0)
+
+summary = re.search(
+    r"(?<=Summary:\n)[\S\s]+(?=EndSummary\n\"\"\")",
+    "".join(source_code),
+).group(0)
+
+code = re.search(
+    r"\"\"\"\n\n\n([\s\S]*)",
+    "".join(source_code)
+).group(1)
 
 context = {
     "number": "{{cookiecutter.lab_work_num}}",
@@ -12,6 +29,9 @@ context = {
     "author": "{{cookiecutter.author}}",
     "teacher": "{{cookiecutter.teacher}}",
     "year": "{{cookiecutter.year}}",
+    "goal": goal.strip(),
+    "summary": summary.strip(),
+    "source": code.strip()
 }
 
 template.render(context)
