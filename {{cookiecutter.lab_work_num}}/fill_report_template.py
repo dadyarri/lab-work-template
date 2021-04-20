0daa92a4192336ab705d588f0e29426fa1f044fa
Daniil <dadyarri@gmail.com>
2021-04-20 17:04:44 +0300

    feat: add new fields to report generator

--- {{cookiecutter.lab_work_num}}/fill_report_template.py
+++ {{cookiecutter.lab_work_num}}/fill_report_template.py
@@ -6,8 +6,12 @@ from docxtpl import DocxTemplate
 template = DocxTemplate(Path("report{{cookiecutter.lab_work_num}}.docx"))
 
 context = {
+    "number": "{{cookiecutter.lab_work_num}}",
     "subject": "{{cookiecutter.subject}}",
     "theme": "{{cookiecutter.theme}}",
+    "author": "{{cookiecutter.author}}",
+    "teacher": "{{cookiecutter.teacher}}",
+    "year": "{{cookiecutter.year}}",
 }
 
 template.render(context)
