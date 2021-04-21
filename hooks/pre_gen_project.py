7730d0723bddf144237e0536af92bffedf3c4287
Daniil Golubev <dadyarri@gmail.com>
2021-04-21 11:51:45 +0300

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import platform
 
 if platform.system() == "Windows":
+  print("Windows pre hook")
   config_path = Path("../cookiecutter.json")
   config = json.load(cookiecutter_config.open(encoding="UTF-8"))
   
